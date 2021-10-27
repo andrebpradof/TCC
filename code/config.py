0e72c5a497b4c25c45576f89e53bf3ab266817ee
andrebpradof <andrebpf@usp.br>
2021-10-27 12:07:41 -0300

    add data

--- code/config.py
+++ code/config.py
@@ -8,7 +8,7 @@ n_epoch = 20
 saves_step = 10
 sig = 1.0
 
-#label_smooth = 0.0
+label_smooth = 0.0
 
 d_epoch = 15
 g_epoch = 5
@@ -29,4 +29,4 @@ pretrain_node_emb_filename_g = '../pre_train/node_clustering/' + dataset + '_pre
 emb_filenames = ['../results/' + dataset + '_gen.emb',
                  '../results/' + dataset + '_dis.emb']
 
-#model_log = '../log/'
+model_log = '../log/'
